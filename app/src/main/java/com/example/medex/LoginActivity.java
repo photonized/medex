e501ef9027e15ed8a41c88b5b173115fb2f4d750
Vlad <vjidk018@uottawa.ca>
2019-10-07 12:15:01 -0400

    Login Page XML
    Finished the Login Page XML

--- app/src/main/java/com/example/medex/LoginActivity.java
+++ app/src/main/java/com/example/medex/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.medex;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Screen Size Info
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenHeight = displaymetrics.heightPixels;
+        int screenWidth = displaymetrics.widthPixels;
     }
 }
--- app/src/main/java/com/example/medex/MainActivity.java
+++ app/src/main/java/com/example/medex/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.medex;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
     }
 }
