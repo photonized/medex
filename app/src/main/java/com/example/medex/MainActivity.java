3f0330bb5e41850ebc5ec116b224fc6c8842bfb4
Vlad <vjidk018@uottawa.ca>
2019-10-07 12:30:38 -0400

    Sign Up Activity
    Sign Up UI created

--- app/src/main/java/com/example/medex/MainActivity.java
+++ app/src/main/java/com/example/medex/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
     }
 }
