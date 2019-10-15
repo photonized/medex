b86bc4dbc1c0aed8fbffc78ce2d8a9f2a9e1f539
omersayshi <brozkiomer@gmail.com>
2019-10-15 19:34:21 -0400

    Login checks with the database if the username exitst

--- app/src/main/java/com/seg/medex/LoginActivity.java
+++ app/src/main/java/com/seg/medex/LoginActivity.java
@@ -1,13 +1,47 @@
 package com.seg.medex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Query;
+
+
+/**
+ * The activity for the Log in page.
+ */
 
 public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Username/Email text field.
+     */
+    private EditText username;
+
+    /**
+     * Password text field.
+     */
+    private EditText password;
+
+    /**
+     * The Firebase Firestore database object.
+     */
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +53,50 @@ public class LoginActivity extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         int screenHeight = displaymetrics.heightPixels;
         int screenWidth = displaymetrics.widthPixels;
+
+        //initializing the variables we are using
+        this.db = FirebaseFirestore.getInstance();
+
+        this.username = findViewById(R.id.username);
+        this.username.setText("");
+
+        this.password = findViewById(R.id.password);
+        this.password.setText("");
+    }
+
+    public void onLogInClick(View view){
+        final String usernameText = username.getText().toString();
+        final String passwordText = password.getText().toString();
+
+        db.collection("users").whereEqualTo("username", usernameText.toLowerCase())
+        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot query = task.getResult();
+                    if (!(query.isEmpty())) {
+                        //username exists
+                        Log.d("LOGIN", "Document exists");
+                        //remove successfullogin and check if the password is the same for the document in the query
+                        successfulLogin();
+                    } else {
+                        //username doesnt exist
+                        Log.d("LOGIN", "No such document");
+                        notsuccessfulLogin();
+                    }
+                } else {
+                    Log.d("LOGIN", "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+    }
+    private void successfulLogin() {
+        Toast.makeText(this, "Success! Signed up and logged in.", Toast.LENGTH_SHORT).show();
+    }
+    private void notsuccessfulLogin() {
+        Toast.makeText(this, "bohoooooo", Toast.LENGTH_SHORT).show();
     }
 
     public void signUpInstead(View view) {
