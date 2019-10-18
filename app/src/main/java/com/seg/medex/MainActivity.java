cca03d71dc27c140d29d30f20db330c0753dba75
Vlad <vjidk018@uottawa.ca>
2019-10-18 00:09:07 -0400

    Asynchronous Fix
    Made firebase calls work by embracing their asynchronous nature

--- app/src/main/java/com/seg/medex/MainActivity.java
+++ app/src/main/java/com/seg/medex/MainActivity.java
@@ -12,11 +12,10 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.Query;
 
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
      * The Firebase Firestore database object.
      */
     private FirebaseFirestore db;
-    private boolean auto_login = false;
 
     SharedPreferences preferences;
 
@@ -34,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         preferences = getSharedPreferences("ID", 0);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        Log.d("VLAD!!!!!!!!!", Arrays.asList(preferences.getAll().values().toArray()).toString());
 
         if(!preferences.contains("username")) {
+            Log.d("VLAD!!!!!!!!!", "username");
             startActivity(new Intent(this, SignupActivity.class));
             finish();
             return;
         }
 
         if(!preferences.getBoolean("logged_in", false)) {
+            Log.d("VLAD!!!!!!!!!", "logged_in");
             startActivity(new Intent(this, LoginActivity.class));
             finish();
             return;
@@ -50,12 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         if(preferences.contains("username") && preferences.getBoolean("logged_in", false)) {
 
-            if(!preferences.getBoolean("completed_profile", false)) {
-                startActivity(new Intent(this, ProfileActivity.class));
-                finish();
-                return;
-
-            } else {
+                Log.d("VLAD!!!!!!!!!", "else");
 
                 this.db = FirebaseFirestore.getInstance();
                 db.collection("users").whereEqualTo("username", preferences.getString("username", ""))
@@ -69,35 +65,51 @@ public class MainActivity extends AppCompatActivity {
                                 if(preferences.getString("username", "").equals(query.getDocuments().get(0).get("username"))
                                 && preferences.getString("password", "").equals(query.getDocuments().get(0).get("password"))
                                 && preferences.getString("email", "").equals(query.getDocuments().get(0).get("email"))) {
-                                    auto_login = true;
+                                    if(!preferences.getBoolean("completed_profile", false)) {
+                                        sendToProfile();
+                                    } else {
+                                        sendToLanding();
+                                    }
                                 }
 
                             }
                         } else {
+                            Log.d("VLAD!!!!!!!!!", "failed");
                             SharedPreferences.Editor editor = preferences.edit();
                             editor.clear();
                             editor.apply();
                             Log.d("LOGIN", "AutoLogin failed", task.getException());
+                            sendToLogin();
                         }
 
                     }
                 });
-                if(auto_login) {
-                    System.out.println("outer");
-                    startActivity(new Intent(this, LandingActivity.class));
-                    finish();
-                    return;
-                }
 
-            }
-
-            System.out.println("outer");
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
+            Log.d("VLAD!!!!!!!!!", "outer");
             return;
         }
 
+        System.out.println(Arrays.asList(preferences.getAll().values().toArray()));
+        System.out.println("huh");
         Toast.makeText(this, "You're not supposed to be here. Report this bug to a developer.", Toast.LENGTH_SHORT);
+        finish();
+    }
+
+    private void sendToLanding() {
+        Log.d("VLAD!!!!!!!!!", "landing");
+        startActivity(new Intent(this, LandingActivity.class));
+        finish();
+    }
+
+    private void sendToLogin() {
+        Log.d("VLAD!!!!!!!!!", "landing");
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    private void sendToProfile() {
+        Log.d("VLAD!!!!!!!!!", "completed_profile");
+        startActivity(new Intent(this, ProfileActivity.class));
         finish();
     }
 }
