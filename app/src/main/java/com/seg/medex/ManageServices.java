a0cc69e7d53c627aec8d2aca4f0929267af14b5c
Vlad <vjidk018@uottawa.ca>
2019-11-07 22:36:20 -0500

    hey ahahaaaa u up?

--- app/src/main/java/com/seg/medex/ManageServices.java
+++ app/src/main/java/com/seg/medex/ManageServices.java
@@ -1,18 +1,62 @@
 package com.seg.medex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class ManageServices extends AppCompatActivity {
 
-    RecyclerView scrollView;
+
+    private ListView list;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_services);
 
+        this.list = findViewById(R.id.services_list);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        final ArrayList<String> elements = new ArrayList<>();
+        db.collection("services")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult() != null) {
+                        Log.d("This", String.valueOf(task.getResult().getDocuments().size()));
+                        for (int i = 0; i < task.getResult().getDocuments().size(); i++) {
+                                Log.d("AAAA", "AAAA");
+                                elements.add(task.getResult().getDocuments().get(i).get("name").toString());
+                                adapter.add(task.getResult().getDocuments().get(i).get("name").toString());
+                                list.setAdapter(adapter);
+                        }
+                    }
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Manage Clinics: ", "Failed. Contact a developer.");
+                    }
+                });
     }
 }
