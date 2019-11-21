4fc520fcff213f2aa8cad2051fcfb459d2d1f7c0
Vlad <vjidk018@uottawa.ca>
2019-11-20 19:56:18 -0500

    added xml for view list of working hours

---
+++ app/src/main/java/com/seg/medex/ClinicTimeActivity.java
@@ -0,0 +1,14 @@
+package com.seg.medex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClinicTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_time);
+    }
+}
