74f475801596f98589505023b2cbb3abdb29ce8c
Vlad <vjidk018@uottawa.ca>
2019-11-26 12:40:02 -0500

    a

--- app/src/main/java/com/seg/medex/UserClinicViewActivity.java
+++ app/src/main/java/com/seg/medex/UserClinicViewActivity.java
@@ -2,6 +2,7 @@ package com.seg.medex;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,11 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,12 +20,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,7 +37,7 @@ public class UserClinicViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_clinics);
+        setContentView(R.layout.activity_user_clinic_view);
 
         this.list = findViewById(R.id.clinic_list);
 
@@ -57,13 +54,17 @@ public class UserClinicViewActivity extends AppCompatActivity {
                         Log.d("This", String.valueOf(task.getResult().getDocuments().size()));
                         for (int i = 0; i < task.getResult().getDocuments().size(); i++) {
                             Log.d("AAAA", String.valueOf(task.getResult().getDocuments().get(i).get("account_type").getClass().getName()));
-                            if (task.getResult().getDocuments().get(i).get("account_type").equals(Long.valueOf(1))) {
+                            if (task.getResult().getDocuments().get(i).get("account_type").equals(Long.valueOf(1)) &&
+                                task.getResult().getDocuments().get(i).get("clinic_name")!=null &&
+                                !task.getResult().getDocuments().get(i).get("clinic_name").equals("") &&
+                                !((ArrayList)task.getResult().getDocuments().get(i).get("services")).isEmpty()) {
                                 Log.d("AAAA", "AAAA");
                                 List currentList = new ArrayList();
                                 currentList.add(0,task.getResult().getDocuments().get(i).get("clinic_name").toString());
-                                currentList.add(1,(task.getResult().getDocuments().get(i).get("street_number").toString() + " " + task.getResult().getDocuments().get(i).get("street_name").toString()));
+                                currentList.add(1,(task.getResult().getDocuments().get(i).get("street_address").toString()));
                                 currentList.add(2,task.getResult().getDocuments().get(i).get("services"));
                                 currentList.add(3,task.getResult().getDocuments().get(i).get("rating").toString());
+                                currentList.add(4, task.getResult().getDocuments().get(i).get("username").toString());
 
                                 elements.add(currentList);
                                 setAdapter(elements);
@@ -84,15 +85,19 @@ public class UserClinicViewActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String clinicName = elements.get(i)[0];
-                String clinicAddress = elements.get(i)[1];
-                ArrayList<String> clinicSerices = elements.get(i)[2];
-                Long clinicRating = elements.get(i)[3];
+                String username = elements.get(i).get(4).toString();
+                openClinic(username);
             }
         });
     }
 
-    private void setAdapter(ArrayList<String> elements) {
+    private void openClinic(String clinicName) {
+        Intent intent = new Intent(this, UserOpenClinicActivity.class);
+        intent.putExtra("clinic_username", clinicName);
+        startActivity(intent);
+    }
+
+    private void setAdapter(ArrayList<List> elements) {
         adapter = new CustomAdapter(this, elements);
         list.setAdapter(adapter);
     }
@@ -132,16 +137,29 @@ public class UserClinicViewActivity extends AppCompatActivity {
             View view = convertView;
             if(view == null) {
                 LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.service_item, null);
+                view = inflater.inflate(R.layout.user_clinic_list_item, null);
             }
 
             String clinicName = list.get(position).get(0).toString();
-            String address = list.get(position).get(1).toString();
+            String addressName = list.get(position).get(1).toString();
             ArrayList<String> services = (ArrayList) list.get(position).get(2);
             String rating = list.get(position).get(3).toString();
 
             TextView nameText = view.findViewById(R.id.name_info);
-            nameText.setText(service);
+            nameText.setText("Name: " + clinicName);
+
+            TextView addressNameText = view.findViewById(R.id.address_info);
+            addressNameText.setText(addressName);
+
+            TextView serviceText = view.findViewById(R.id.service_info);
+            String string = "";
+            for(String service : services) {
+                string += service + ",";
+            }
+            serviceText.setText(string);
+
+            TextView ratingText = view.findViewById(R.id.rating_text);
+            ratingText.setText(rating);
 
             return view;
         }
