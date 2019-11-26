d302b3312e661557ec1762672ce70961332cc060
omersayshi <brozkiomer@gmail.com>
2019-11-25 22:20:00 -0500

    finished working with reserve spot/book appointment Nader will take it from here

--- app/src/main/java/com/seg/medex/UserReserveSpot.java
+++ app/src/main/java/com/seg/medex/UserReserveSpot.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class UserReserveSpot extends AppCompatActivity {
+
+    private CalendarView calender;
+    private TextView showSelectedDay;
+    private Spinner selectedTime;
+    private String clinicUserName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_reserve_spot);
+
+        this.calender = findViewById(R.id.calendarView);
+        this.showSelectedDay = findViewById(R.id.selectedDayText);
+        this.selectedTime = findViewById(R.id.time_spinner);
+        this.clinicUserName = (String) getIntent().getSerializableExtra("clinic_username");
+
     }
 }
